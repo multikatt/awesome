e2adf6b5d12a2d2d19c8455531f56a4d3cae0230
david <david@multikaos.be>
2011-09-03 18:07:00 +0200

    Use batproc from contrib instead.
    This because /sys/class shows the fully charged battery as UNKNOWN.

--- wi.lua
+++ wi.lua
@@ -1,4 +1,5 @@
 require("vicious")
+require("vicious.contrib")
 
 spacer = widget({ type = "textbox" })
 spacer.text = "::"
@@ -8,19 +9,20 @@ batterytext = widget({ type ="textbox" })
 battery = awful.widget.progressbar()
 battery:set_width(100)
 battery:set_ticks(10)
-vicious.register(battery, vicious.widgets.bat,
+--vicious.register(battery, vicious.widgets.bat,
+vicious.register(battery, vicious.contrib.batproc,
         function (widget, args)
-        if args[1] == "+" then
-            if args[2] < 20 then
-                battery:set_color("#FF0000")
-            else battery:set_color("#28ec51")
+        battery:set_color("#28ec51")
+
+        if args[2] < 20 then
+            if args[1] == "-" then
+            battery:set_color("#FF0000")
             end
+        end
+
+        if args[1] == "↯" then
             batterytext.text = ""
         else
-            if args[2] < 20 then
-                battery:set_color("#FF0000")
-            else battery:set_color("#28ec51")
-            end
             batterytext.text = args[1]
         end
 
